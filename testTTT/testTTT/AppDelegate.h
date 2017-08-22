02f445dd09d5b34a1e75b470d9ced4586d357134
changxinya <cxyicxy@126.com>
2017-08-22 19:18:38 +0800

    2017082219:18

---
+++ testTTT/testTTT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testTTT
+//
+//  Created by changxinya on 2017/8/22.
+//  Copyright © 2017年 changxinya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testTTT/testTTT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testTTT
+//
+//  Created by changxinya on 2017/8/22.
+//  Copyright © 2017年 changxinya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
